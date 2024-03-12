2c437a25795d4e6edd5bcd2b47b55c9df348df96
Muhammed Ershad P <muhammedershadp@gmail.com>
2024-03-12 22:53:09 +0530

    first commit

---
+++ my-react-app/tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+     extend: {},
+  },
+  variants: {
+     extend: {},
+  },
+  plugins: [addVariablesForColors],
+ }
+ 
+ function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
